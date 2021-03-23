b5745b1e8385c5c317477cf730a1e1cfcebb429b
bratside5 <bratside5@gmail.com>
2021-03-23 08:22:41 +0100

    admin

--- admin/client/src/components/AdminPage.js
+++ admin/client/src/components/AdminPage.js
@@ -106,50 +106,6 @@ const AdminPage = ({ user }) => {
               </div>
             </div>
           </div>
-          <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-            <div class="bg-gray-700 border border-gray-400 rounded shadow p-2">
-              <div class="flex flex-row items-center">
-                <div class="flex-shrink pr-4">
-                  <div class="rounded p-3 bg-red-600">
-                    <i class="fas fa-users fa-2x fa-fw fa-inverse"></i>
-                  </div>
-                </div>
-                <div class="flex-1  md:text-center">
-                  <h5 class="font-bold uppercase text-gray-200">
-                    Registrations This Month
-                  </h5>
-                  <h3 class="font-bold text-3xl text-gray-300">
-                    <GetDataAll />
-                    <span class="text-blue-500">
-                      <i class="fas fa-exchange-alt"></i>
-                    </span>
-                  </h3>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-            <div class="bg-gray-700 border border-gray-400 rounded shadow p-2">
-              <div class="flex flex-row items-center">
-                <div class="flex-shrink pr-4">
-                  <div class="rounded p-3 bg-indigo-600">
-                    <i class="fas fa-user-plus fa-2x fa-fw fa-inverse"></i>
-                  </div>
-                </div>
-                <div class="flex-1  md:text-center">
-                  <h5 class="font-bold uppercase text-gray-200">
-                    Registrations Past 6 Months
-                  </h5>
-                  <h3 class="font-bold text-3xl text-gray-300 py-6">
-                    2{" "}
-                    <span class="text-indigo-600">
-                      <i class="fas fa-caret-up"></i>
-                    </span>
-                  </h3>
-                </div>
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
 
